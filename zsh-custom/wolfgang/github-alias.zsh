3a20c7209d135bd5b860450f74d4bcbadc5fbf90
Wolfgang Therrien <wolfgang.therrien@trajector.com>
2022-02-21 10:17:51 -0500

    Typo.

--- zsh-custom/wolfgang/github-alias.zsh
+++ zsh-custom/wolfgang/github-alias.zsh
@@ -1,4 +1,4 @@
-alias gh_main="git checkout main && git pull origin/main"
+alias gh_main="git checkout main && git pull origin main"
 alias gh_cleanup_merged="git fetch --prune && git branch --merged | grep -v '\*\|master\|main\|develop' | xargs -n 1 git branch -d"
 alias gh_rebase="git rebase -i origin/main"
 
